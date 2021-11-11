2ebe7b90f793a72018c03468784261d2c9e53a59
Robustprogram <robustprogram@users.noreply.github.com>
2021-11-11 19:50:06 +1100

    Write more necessary code

--- ugv_rpi_brain/main.py
+++ ugv_rpi_brain/main.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "the-hat"
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @socketio.on("connect")
