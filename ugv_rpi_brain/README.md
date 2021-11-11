6f03168c5c910b5578bf7772432aececb71a88da
Robustprogram <robustprogram@users.noreply.github.com>
2021-11-11 19:46:52 +1100

    Work on socket side

--- ugv_rpi_brain/README.md
+++ ugv_rpi_brain/README.md
@@ -1,4 +1,7 @@
 ## UGV RPI Brain
+
 ====
 
 This script will build up the socket server, allowing people to send commands to the robotic arm
+
+To start run `poetry run python main.py`
---
+++ ugv_rpi_brain/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "the-hat"
+socketio = SocketIO(app)
+
+
+@socketio.on("connect")
+def test_connect(auth):
+    emit("my response", {"data": "Connected"})
+
+
+@socketio.on("disconnect")
+def test_disconnect():
+    print("Client disconnected")
+
+
+if __name__ == "__main__":
+    socketio.run(app)
