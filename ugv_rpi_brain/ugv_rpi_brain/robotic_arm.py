4d907564504be3ee6e4e5d99525fe3cacdf4de84
Robustprogram <robustprogram@users.noreply.github.com>
2021-11-14 17:29:03 +1100

    With delay

--- ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
+++ ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 
 def test_movement():
@@ -16,6 +17,7 @@ def test_movement():
                 ser.write(str(",").encode("utf-8"))
                 ser.write(str("360").encode("utf-8"))
                 ser.write(str("\n").encode("utf-8"))
+                time.sleep(5)
                 ser.close()
             else:
                 print(line)
