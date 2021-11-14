18cb39f6a450600b73553f62c9798db7ae93840e
Robustprogram <robustprogram@users.noreply.github.com>
2021-11-14 17:17:32 +1100

    Does this work?

--- ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
+++ ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
@@ -4,9 +4,11 @@ import serial
 def test_movement():
     ser = serial.Serial("/dev/ttyACM0", 9600)
     ser.flush()
-    ser.write(str("rotate").encode("utf-8"))
-    ser.write(str(",").encode("utf-8"))
-    ser.write(str("180").encode("utf-8"))
-    ser.write(str(",").encode("utf-8"))
-    ser.write(str("360").encode("utf-8"))
-    ser.write(str("\n").encode("utf-8"))
+
+    while ser.in_waiting > 0:
+        ser.write(str("rotate").encode("utf-8"))
+        ser.write(str(",").encode("utf-8"))
+        ser.write(str("180").encode("utf-8"))
+        ser.write(str(",").encode("utf-8"))
+        ser.write(str("360").encode("utf-8"))
+        ser.write(str("\n").encode("utf-8"))
