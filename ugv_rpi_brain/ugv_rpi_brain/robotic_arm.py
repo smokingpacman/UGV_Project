3968289074cd6b4d47095ee463b7ba33429edc7d
Robustprogram <robustprogram@users.noreply.github.com>
2021-11-14 17:21:17 +1100

    More modifications

--- ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
+++ ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
@@ -6,9 +6,16 @@ def test_movement():
     ser.flush()
 
     while True:
-        ser.write(str("rotate").encode("utf-8"))
-        ser.write(str(",").encode("utf-8"))
-        ser.write(str("180").encode("utf-8"))
-        ser.write(str(",").encode("utf-8"))
-        ser.write(str("360").encode("utf-8"))
-        ser.write(str("\n").encode("utf-8"))
+        if ser.in_waiting > 0:
+            line = ser.readline().decode("utf-8").rstrip()
+
+            if line == "request":
+                ser.write(str("rotate").encode("utf-8"))
+                ser.write(str(",").encode("utf-8"))
+                ser.write(str("180").encode("utf-8"))
+                ser.write(str(",").encode("utf-8"))
+                ser.write(str("360").encode("utf-8"))
+                ser.write(str("\n").encode("utf-8"))
+            else:
+                print(line)
+                break
