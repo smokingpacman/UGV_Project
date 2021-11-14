59e36681939b0d43531294239cbfd1f2a210b7a4
Robustprogram <robustprogram@users.noreply.github.com>
2021-11-14 17:32:41 +1100

    Convert into a single line

--- ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
+++ ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
@@ -2,9 +2,4 @@ from . import serial_connection
 
 
 def test_movement():
-    serial_connection.write(str("rotate").encode("utf-8"))
-    serial_connection.write(str(",").encode("utf-8"))
-    serial_connection.write(str("180").encode("utf-8"))
-    serial_connection.write(str(",").encode("utf-8"))
-    serial_connection.write(str("360").encode("utf-8"))
-    serial_connection.write(str("\n").encode("utf-8"))
+    serial_connection.write(str("rotate,180,360\n").encode("utf-8"))
