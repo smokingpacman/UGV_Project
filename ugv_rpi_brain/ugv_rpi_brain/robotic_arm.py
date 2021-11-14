8b8dc4d96c1fde5d02bfb44e65604f5dff008033
Robustprogram <robustprogram@users.noreply.github.com>
2021-11-14 17:24:48 +1100

    Testing with with

--- ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
+++ ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
@@ -2,20 +2,19 @@ import serial
 
 
 def test_movement():
-    ser = serial.Serial("/dev/ttyACM0", 9600)
-    ser.flush()
-
     while True:
         if True:
-            line = ser.readline().decode("utf-8").rstrip()
+            with serial.Serial("/dev/ttyACM0", 9600) as ser:
+                ser.flush()
+                line = ser.readline().decode("utf-8").rstrip()
 
-            if line == "request":
-                ser.write(str("rotate").encode("utf-8"))
-                ser.write(str(",").encode("utf-8"))
-                ser.write(str("180").encode("utf-8"))
-                ser.write(str(",").encode("utf-8"))
-                ser.write(str("360").encode("utf-8"))
-                ser.write(str("\n").encode("utf-8"))
-            else:
-                print(line)
-                break
+                if line == "request":
+                    ser.write(str("rotate").encode("utf-8"))
+                    ser.write(str(",").encode("utf-8"))
+                    ser.write(str("180").encode("utf-8"))
+                    ser.write(str(",").encode("utf-8"))
+                    ser.write(str("360").encode("utf-8"))
+                    ser.write(str("\n").encode("utf-8"))
+                else:
+                    print(line)
+                    break
