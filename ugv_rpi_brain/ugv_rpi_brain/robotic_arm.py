17ba68e98e6c406583db98426624f0fb3be1f927
Robustprogram <robustprogram@users.noreply.github.com>
2021-11-14 17:26:46 +1100

    This should break

--- ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
+++ ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
@@ -2,19 +2,21 @@ import serial
 
 
 def test_movement():
-    while True:
-        if True:
-            with serial.Serial("/dev/ttyACM0", 9600) as ser:
-                ser.flush()
-                line = ser.readline().decode("utf-8").rstrip()
+    ser = serial.Serial("/dev/ttyACM0", 9600)
+    ser.flush()
 
-                if line == "request":
-                    ser.write(str("rotate").encode("utf-8"))
-                    ser.write(str(",").encode("utf-8"))
-                    ser.write(str("180").encode("utf-8"))
-                    ser.write(str(",").encode("utf-8"))
-                    ser.write(str("360").encode("utf-8"))
-                    ser.write(str("\n").encode("utf-8"))
-                else:
-                    print(line)
-                    break
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode("utf-8").rstrip()
+
+            if line == "request":
+                ser.write(str("rotate").encode("utf-8"))
+                ser.write(str(",").encode("utf-8"))
+                ser.write(str("180").encode("utf-8"))
+                ser.write(str(",").encode("utf-8"))
+                ser.write(str("360").encode("utf-8"))
+                ser.write(str("\n").encode("utf-8"))
+                ser.close()
+            else:
+                print(line)
+                break
