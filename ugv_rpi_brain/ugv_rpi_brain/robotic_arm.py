87d58fd639c9a450e4b0cda862210369835cac71
Robustprogram <robustprogram@users.noreply.github.com>
2021-11-14 16:50:45 +1100

    Update code

--- ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
+++ ugv_rpi_brain/ugv_rpi_brain/robotic_arm.py
@@ -3,4 +3,10 @@ import serial
 
 def test_movement():
     with serial.Serial("/dev/ttyACM0", 9600) as serial_open:
-        serial_open.write("rotate,45,45".encode("utf-8"))
+        serial_open.flush()
+        serial_open.write("rotate".encode("utf-8"))
+        serial_open.write(",".encode("utf-8"))
+        serial_open.write("180".encode("utf-8"))
+        serial_open.write(",".encode("utf-8"))
+        serial_open.write("360".encode("utf-8"))
+        serial_open.write("\n".encode("utf-8"))
