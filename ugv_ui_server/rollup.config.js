57c94002d0c5a08ee923c3c91ebb18d3eacc0248
Robustprogram <robustprogram@users.noreply.github.com>
2021-12-11 14:49:57 +1100

    Separate production and development configs

--- ugv_ui_server/rollup.config.js
+++ ugv_ui_server/rollup.config.js
@@ -1,12 +1,20 @@
 import eslint from '@rollup/plugin-eslint';
+import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import run from '@rollup/plugin-run';
+
+const isProduction = process.env.BUILD === 'production';
 
 export default {
   input: 'src/index.ts',
   output: {
-    dir: 'dist',
+    dir: isProduction ? 'dist' : 'dev',
     format: 'cjs',
   },
-  plugins: [eslint(), typescript(), run()],
+  plugins: [
+    eslint(),
+    typescript(),
+    isProduction && terser(),
+    !isProduction && run(),
+  ],
 };
